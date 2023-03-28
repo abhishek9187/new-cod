10b06c3d33d99f376faa5a3877e16e4f9caff12f
Abhishek9187 <ingaleabhishek10@gmail.com>
2023-03-28 23:07:05 +0530

    message

---
+++ first program.py
@@ -0,0 +1,11 @@
+a=5
+b=13
+c=a+b
+print("cc")
+print("hello world")
+name=float(input("enter a number n1:"))
+name2=float(input("enter a number n2 :"))
+abhi=name+name2
+print(abhi)
+c=a/b
+print(float(c))
